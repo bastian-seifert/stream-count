d052f185f94b7fd8f9e26fd4741a13691089d290
Bastian Seifert <bastian.seifert@freenet.de>
2024-06-23 23:14:46 +0200

    start using proptest stateful for testing ElementSet ops

--- src/elementset.rs
+++ src/elementset.rs
@@ -1,8 +1,19 @@
 use std::{collections::HashSet, hash::Hash};
 
+/// The ElementSet trait defines the operations needed
+/// for the stream count algorithm to operate (a subset
+/// of the operations availabel on hash sets).
+///
+/// This trait allows flexibility for the user (e.g., choose
+/// whatever data structure fits your use case best) as well
+/// as enabling snapshot testing (as hashsets are non-deterministic).
 pub trait ElementSet {
     type Element;
-    fn with_capacity(capacity: usize) -> Self;
+    fn with_capacity(capacity: usize) -> Self
+    where
+        Self: Sized;
+    /// Inserts `elem` if it is not in `self`,
+    /// otherwise does nothing.
     fn insert(&mut self, elem: Self::Element);
     fn contains(&self, elem: &Self::Element) -> bool;
     fn remove(&mut self, elem: &Self::Element);
@@ -44,6 +55,8 @@ where
     }
 }
 
+/// This implementation is very inefficient and should
+/// only used for snapshot testing.
 impl<T> ElementSet for Vec<T>
 where
     T: Eq,
@@ -77,5 +90,78 @@ where
 
     fn iter(&self) -> impl Iterator<Item = &T> {
         self.as_slice().iter()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::marker::PhantomData;
+    use std::{fmt::Debug, time::Duration};
+
+    use async_trait::async_trait;
+    use proptest::prelude::*;
+    use proptest_stateful::{ModelState, ProptestStatefulConfig};
+
+    use super::ElementSet;
+
+    #[derive(Clone, Debug)]
+    enum ElementSetOps {
+        Insert(u32),
+        Remove(u32),
+    }
+    #[derive(Clone, Debug, Default)]
+    struct TestState<E: ElementSet + Clone + Default> {
+        marker: PhantomData<E>,
+    }
+
+    #[async_trait(?Send)]
+    impl<E> ModelState for TestState<E>
+    where
+        E: ElementSet<Element = u32> + Clone + Default + Debug,
+    {
+        type Operation = ElementSetOps;
+        type RunContext = E;
+        type OperationStrategy = BoxedStrategy<Self::Operation>;
+
+        fn op_generators(&self) -> Vec<Self::OperationStrategy> {
+            // For each step test, arbitrarily pick Insert or Remove, regardless of the test state:
+            // TODO: Add strategy to gen numbers
+            vec![
+                Just(ElementSetOps::Insert(0)).boxed(),
+                Just(ElementSetOps::Remove(0)).boxed(),
+            ]
+        }
+
+        // No preconditions to worry about or test state to maintain yet
+        fn preconditions_met(&self, _op: &Self::Operation) -> bool {
+            true
+        }
+        fn next_state(&mut self, _op: &Self::Operation) {}
+
+        async fn init_test_run(&self) -> Self::RunContext {
+            E::with_capacity(100)
+        }
+
+        async fn run_op(&self, op: &Self::Operation, ctxt: &mut Self::RunContext) {
+            match op {
+                ElementSetOps::Insert(val) => ctxt.insert(*val),
+                ElementSetOps::Remove(val) => ctxt.remove(val),
+            }
+        }
+
+        async fn check_postconditions(&self, _ctxt: &mut Self::RunContext) {}
+        async fn clean_up_test_run(&self, _ctxt: &mut Self::RunContext) {}
+    }
+
+    #[test]
+    fn run_cases_vec() {
+        let config = ProptestStatefulConfig {
+            min_ops: 10,
+            max_ops: 50,
+            test_case_timeout: Duration::from_secs(60),
+            proptest_config: ProptestConfig::default(),
+        };
+
+        proptest_stateful::test::<TestState<Vec<u32>>>(config);
     }
 }
